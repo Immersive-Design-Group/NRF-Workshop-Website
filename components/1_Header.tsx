1b9a1c4668dd213e5fc03f4218cc9c42166a16b5
Chihero0 <Weitaojiangdesign@gmail.com>
2025-10-04 22:32:07 +0900

    update

--- components/1_Header.tsx
+++ components/1_Header.tsx
@@ -9,34 +9,55 @@ export default function Header() {
           <div className="flex items-center gap-8 mt-8">
             {/* SUSTech Logo Group */}
             <div className="flex items-center">
-              <Image
-                src="/NRF-Workshop-Website/Assets/logo/sustech.svg"
-                alt="SUSTech Logo"
-                width={160}
-                height={32}
-                className="object-contain"
-              />
+              <a 
+                href="https://www.sustech.edu.cn/en/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="hover:opacity-80 transition-opacity"
+              >
+                <Image
+                  src="/NRF-Workshop-Website/Assets/logo/sustech.svg"
+                  alt="SUSTech Logo"
+                  width={160}
+                  height={32}
+                  className="object-contain"
+                />
+              </a>
             </div>
             
             {/* School of Design Logo Group */}
             <div className="flex items-center">
-              <Image
-                src="/NRF-Workshop-Website/Assets/logo/sd.svg"
-                alt="School of Design Logo"
-                width={125}
-                height={32}
-                className="object-contain"
-              />
+              <a 
+                href="https://designschool.sustech.edu.cn" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="hover:opacity-80 transition-opacity"
+              >
+                <Image
+                  src="/NRF-Workshop-Website/Assets/logo/sd.svg"
+                  alt="School of Design Logo"
+                  width={125}
+                  height={32}
+                  className="object-contain"
+                />
+              </a>
             </div>
             
             {/* KAIST Logo */}
-            <Image
-              src="/NRF-Workshop-Website/Assets/logo/kaist.png"
-              alt="KAIST Logo"
-              width={110}
-              height={32}
-              className="object-contain"
-            />
+            <a 
+              href="https://www.kaist.ac.kr/kr/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="hover:opacity-80 transition-opacity"
+            >
+              <Image
+                src="/NRF-Workshop-Website/Assets/logo/kaist.png"
+                alt="KAIST Logo"
+                width={110}
+                height={32}
+                className="object-contain"
+              />
+            </a>
           </div>
         </div>
       </div>
