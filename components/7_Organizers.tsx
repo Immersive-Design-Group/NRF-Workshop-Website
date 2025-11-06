667e1bc0c5a06eba0abb630177cc25afd52e4068
Chihero0 <Weitaojiangdesign@gmail.com>
2025-11-07 00:47:42 +0800

    1

--- components/7_Organizers.tsx
+++ components/7_Organizers.tsx
@@ -53,7 +53,7 @@ export default function Organizers() {
           </div>
         </div>
         
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
           {organizers.map((organizer, index) => (
             <div key={index} className="text-center">
               {organizer.url ? (
