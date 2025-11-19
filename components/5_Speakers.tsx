b67be7b9a62179304b95e8fdf70be69eb98c17c1
Chihero0 <Weitaojiangdesign@gmail.com>
2025-11-19 10:24:01 +0800

    1

--- components/5_Speakers.tsx
+++ components/5_Speakers.tsx
@@ -19,7 +19,7 @@ export default function Speakers() {
     {
       name: 'Boyu Gao',
       title: 'Associate Professor',
-      affiliation: 'College of Information Science and Technology & College of Cyber Security, Jinan University',
+      affiliation: 'College of Cyber Security/Guangdong Institute of Smart Education, Jinan University',
       url: 'https://boyugao.wordpress.com'
     },
     {
